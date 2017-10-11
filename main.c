08e393caebd974eab732d1141d01fc7199be6541
박현묵 <mook1993@naver.com>
2017-10-11 17:29:40 +0900

    곱하기 구현

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
- 
+
 int add(int, int);
 int sub(int, int);
 int div(int, int);
 int multi(int, int);
- 
+
 int main(int argc, char* argv[])
 {
 	int a, b, c;
 	char operator;
-	
+
 	printf("Please enter a expression");
 	scanf("%d %c %d", &a, &operator, &b);
-	
+
 	switch (operator)
 	{
 		case '+':
@@ -28,30 +28,30 @@ int main(int argc, char* argv[])
 			c = div(a, b);
 			break;
 	}
-	
+
 	printf("result is %d.\n", c);
-	
+
 	return 0;
 }
- 
+
 int add(int a, int b)
 {
 	//TODO implementation
 	return 0;
 }
- 
+
 int sub(int a, int b)
 {
 	//TODO implementation
 	return 0;
 }
- 
+
 int multi(int a, int b)
 {
 	//TODO implementation
-	return 0;
+	return a*b;
 }
- 
+
 int div(int a, int b)
 {
 	//TODO implementation
